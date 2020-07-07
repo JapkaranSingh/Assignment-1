13b6f94f10597aecb3026ccf890dd14c19c9f085
JapkaranSingh <japkaransingh2@gmail.com>
2020-07-06 22:24:53 -0400

    index.js Updated

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Index' });
+  res.render('index');
 });
 
+/* GET About Page */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about');
+});
+
+/* GET Services Page */
+router.get('/services', function(req, res, next) {
+  res.render('services');
+});
+
+/* GET Project Page */
+router.get('/projects', function(req, res, next) {
+  res.render('project');
+});
+
+/* GET Contact Page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
 });
 
 module.exports = router;
